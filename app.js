007df5ae650de1f7674ce09af6494514ffce6817
RishabhPathak1211 <rishabh2000.pathak@gmail.com>
2021-02-13 20:19:16 +0530

    deployment changes

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const ExpressError = require('./utils/ExpressError');
+const indexRoutes = require('./routes/index');
+
+const dbUrl = process.env.DB_URL || 'mongodb://192.168.0.36:27017/tedx';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRoutes);
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something went wrong';
+    res.status(statusCode).render('error', { err });
+});
+
+module.exports = { app, db }
